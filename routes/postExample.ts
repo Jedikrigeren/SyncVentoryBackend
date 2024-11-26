ba5f743f9e2cd72f9a65bba768479db85452657d
Jedikrigeren <hasselsteen95@live.dk>
2024-11-26 11:23:59 +0100

    added a log for post request

--- routes/postExample.ts
+++ routes/postExample.ts
@@ -1,6 +1,12 @@
 export const handlePostExample = async (req: Request): Promise<Response> => {
     try {
+      const { method, url } = req;
+      const timestamp = new Date().toISOString();
+      console.log(`[${timestamp}] ${method} request received for: ${url}`);
+
       const body = await req.json(); // Parse JSON body
+      console.log("Parsed JSON body:", body);
+      
       return new Response(`You posted: ${JSON.stringify(body)}`, {
         headers: { "content-type": "application/json" },
       });
