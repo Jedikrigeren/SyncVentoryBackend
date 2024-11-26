12cfb153f0eafd6d55227b667dfa3f05c93e19fa
Jedikrigeren <hasselsteen95@live.dk>
2024-11-26 14:07:15 +0100

    added cors to router

--- routes.ts
+++ routes.ts
@@ -5,6 +5,16 @@ export const router = (req: Request): Response | Promise<Response> => {
   const url = new URL(req.url);
   const pathname = url.pathname;
 
+  if (req.method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
+
   // Handle GET requests
   if (req.method === "GET" && pathname === "/example-get") {
     return handleGetExample(req);
